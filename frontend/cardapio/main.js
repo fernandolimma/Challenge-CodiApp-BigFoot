9b5254a85d8e4278e06be38bd74549f565c12d19
Bruno Gonçalves Ferreira <brungoncalvesferreira@outlook.com>
2023-07-26 20:49:39 -0300

    :boom: criação dos cards javascript

--- frontend/cardapio/main.js
+++ frontend/cardapio/main.js
@@ -1,6 +1,45 @@
 const buttonOpenHour = document.querySelector(".button-hour")
 const buttonRemoveHour = document.querySelector(".close-hour")
 const body = document.querySelector("body")
+const boxCardapio = document.querySelector(".caixas")
+
+const data = [
+  {
+    image:
+      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+    title: "X-Burguer",
+    description: "Pão, Carne, queijo , molho especial.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+    title: "X-Burguer",
+    description: "Pão, Carne, queijo , molho especial.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+    title: "X-Burguer",
+    description: "Pão, Carne, queijo",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+    title: "X-Burguer",
+    description: "Pão, Carne, queijo , molho especial.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+    title: "X-Burguer",
+    description: "Pão, Carne, queijo , molho especial.",
+    price: 19.99,
+  },
+]
 
 buttonOpenHour.addEventListener("click", () => {
   body.classList.add("show")
@@ -9,3 +48,25 @@ buttonOpenHour.addEventListener("click", () => {
 buttonRemoveHour.addEventListener("click", () => {
   body.classList.remove("show")
 })
+
+const element = data.map((info) => {
+  return `
+    <div class="box-cardapio">
+      <div class="imagem">
+        <img
+          src="${info.image}"
+          alt="Descrição da Imagem"
+        />
+      </div>
+
+      <div class="texto">
+        <h2>${info.title}</h2>
+        <p>${info.description}</p>
+        <strong>${info.price}</strong>
+        <button>Adicionar</button>
+      </div>
+    </div>
+  `
+})
+
+boxCardapio.innerHTML = element.join("")
