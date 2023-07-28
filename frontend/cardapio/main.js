0ef7d5bb7684d211ae8d9fd2572cf6b303817b0e
isaacguedess <isaacguedess10@gmail.com>
2023-07-28 12:45:50 -0300

    :bomm: box do menu em andamento

--- frontend/cardapio/main.js
+++ frontend/cardapio/main.js
@@ -8,38 +8,54 @@ const data = [
     image:
       "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
     title: "X-Burguer",
-    description: "Pão, Carne, queijo , molho especial.",
+    description:  "Pão, Carne, queijo , molho especial.",
+    price: 19.99,
+    
+  
+  },
+  {
+    image:
+      "https://img.freepik.com/fotos-premium/burger-costeleta-dupla-queijo-cheddar-duplo-bacon-cebola-frita-picles-molho-e-pao-artesanal_524291-99.jpg?size=626&ext=jpg&ga=GA1.1.462900268.1690408347&semt=sph",
+    title: "X-Burguer",
+    description: "Pão , Carne , Quiejo , Molho especial",
+    price: 19.99,
+  },
+  {
+   image:
+      "https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg&ga=GA1.2.462900268.1690408347&semt=sph",
+    title: "X-Burguer",
+    description: " Pão ,Carne, Quiejo , molho especial",
     price: 19.99,
   },
   {
     image:
-      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+      "https://img.freepik.com/fotos-premium/hamburguer-com-queijo-cheddar-em-prato-de-madeira_74692-589.jpg?size=626&ext=jpg&ga=GA1.1.462900268.1690408347&semt=sph",
     title: "X-Burguer",
-    description: "Pão, Carne, queijo , molho especial.",
+    description: " Pão ,Carne, Quiejo , molho especial",
     price: 19.99,
   },
   {
     image:
-      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+      "https://img.freepik.com/fotos-premium/hamburguer-com-carne-bacon-queijo-picles-tomate-e-molho-em-um-fundo-preto-de-madeira_524291-655.jpg?size=626&ext=jpg",
     title: "X-Burguer",
-    description: "Pão, Carne, queijo",
+    description: " Pão ,Carne, Quiejo , molho especial",
     price: 19.99,
+
+    
   },
   {
     image:
-      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+      "https://img.freepik.com/fotos-gratis/cheeseburger-gourmet-grelhado-com-legumes-frescos-e-batatas-fritas-geradas-por-ia_188544-38732.jpg?size=626&ext=jpg&ga=GA1.1.462900268.1690408347&semt=ais",
     title: "X-Burguer",
-    description: "Pão, Carne, queijo , molho especial.",
+    description: " Pão ,Carne, Quiejo , molho especial",
     price: 19.99,
-  },
-  {
-    image:
-      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
-    title: "X-Burguer",
-    description: "Pão, Carne, queijo , molho especial.",
-    price: 19.99,
+
+    
   },
 ]
+
+
+
 
 buttonOpenHour.addEventListener("click", () => {
   body.classList.add("show")
@@ -51,6 +67,7 @@ buttonRemoveHour.addEventListener("click", () => {
 
 const element = data.map((info) => {
   return `
+  <div class="box-model">
     <div class="box-cardapio">
       <div class="imagem">
         <img
@@ -58,15 +75,19 @@ const element = data.map((info) => {
           alt="Descrição da Imagem"
         />
       </div>
-
+       
       <div class="texto">
         <h2>${info.title}</h2>
         <p>${info.description}</p>
         <strong>${info.price}</strong>
         <button>Adicionar</button>
-      </div>
+        </div>
+
+    </div>
     </div>
   `
 })
 
 boxCardapio.innerHTML = element.join("")
+
+
