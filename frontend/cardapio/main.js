e93ae34b7628c2d21f84be9538ab101c72ddec27
Bruno Gonçalves Ferreira <brungoncalvesferreira@outlook.com>
2023-08-01 20:12:12 -0300

    :boom: refatorando código

--- frontend/cardapio/main.js
+++ frontend/cardapio/main.js
@@ -1,27 +1,25 @@
-const buttonOpenHour = document.querySelector(".button-hour")
-const buttonRemoveHour = document.querySelector(".close-hour")
-const body = document.querySelector("body")
-const boxCardapio = document.querySelector(".caixas")
+const buttonOpenHour = document.querySelector(".button-hour");
+const buttonRemoveHour = document.querySelector(".close-hour");
+const body = document.querySelector("body");
+const boxCardapio = document.querySelector(".caixas");
 
-const data =[
+const data = [
   {
     image:
       "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
     title: "X-Burguer",
-    description:  "Pão, Carne, queijo , molho especial.",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
     price: 23.99,
-    
-  
   },
   {
     image:
       "https://img.freepik.com/fotos-premium/burger-costeleta-dupla-queijo-cheddar-duplo-bacon-cebola-frita-picles-molho-e-pao-artesanal_524291-99.jpg?size=626&ext=jpg&ga=GA1.1.462900268.1690408347&semt=sph",
-    title: "Duplo burquer",
-    description: "Pão , Carne , Quiejo , bacon , molho especial",
+    title: "Duplo Burguer",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
     price: 26.99,
   },
   {
-   image:
+    image:
       "https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg&ga=GA1.2.462900268.1690408347&semt=sph",
     title: "X-Burguer",
     description: " Pão brioche ,Carne, cheddar , farofa de bacon , salada ",
@@ -31,85 +29,65 @@ const data =[
     image:
       "https://img.freepik.com/fotos-premium/hamburguer-com-queijo-cheddar-em-prato-de-madeira_74692-589.jpg?size=626&ext=jpg&ga=GA1.1.462900268.1690408347&semt=sph",
     title: "X-Burguer",
-    description: " Pão ,Carne, Quiejo , molho especial",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
     price: 19.99,
   },
   {
     image:
       "https://img.freepik.com/fotos-premium/hamburguer-com-carne-bacon-queijo-picles-tomate-e-molho-em-um-fundo-preto-de-madeira_524291-655.jpg?size=626&ext=jpg",
     title: "X-Burguer",
-    description: " Pão ,Carne, Quiejo , molho especial",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
     price: 19.99,
-
-    
   },
   {
     image:
       "https://img.freepik.com/fotos-gratis/cheeseburger-gourmet-grelhado-com-legumes-frescos-e-batatas-fritas-geradas-por-ia_188544-38732.jpg?size=626&ext=jpg&ga=GA1.1.462900268.1690408347&semt=ais",
     title: "X-Burguer",
-    description: " Pão ,Carne, Quiejo , molho especial",
-    price: 19.99,
-
-    
-  },
-  {
-    image:
-      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
-    title: "X-Burguer",
-    description: "Pão, Carne, queijo , molho especial.",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
     price: 19.99,
   },
   {
     image:
       "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
     title: "X-Burguer",
-    description: "Pão, Carne, queijo , molho especial.",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
     price: 19.99,
   },
-]
-
-
-
-
-
+  {
+    image:
+      "https://img.freepik.com/fotos-gratis/cheeseburguer-grelhado-com-tomate-cebola-e-fritas-gerado-por-ia_188544-43039.jpg",
+    title: "X-Burguer",
+    description: "Pão brioche ,Carne, cheddar , farofa de bacon , salada",
+    price: 19.99,
+  },
+];
 
 buttonOpenHour.addEventListener("click", () => {
-  body.classList.add("show")
-})
+  body.classList.add("show");
+});
 
 buttonRemoveHour.addEventListener("click", () => {
-  body.classList.remove("show")
-})
+  body.classList.remove("show");
+});
 
 const element = data.map((info) => {
   return `
   <div class="box-model">
     <div class="box-cardapio">
-      <div class="imagem">
         <img
           src="${info.image}"
           alt="Descrição da Imagem"
         />
-      </div>
-       
       <div class="texto">
         <h2>${info.title}</h2>
         <p>${info.description}</p>
-        <strong>${info.price}</strong>
+        <strong>R$${info.price}</strong>
         <button>Adicionar</button>
-        </div>
+      </div>
 
     </div>
-    </div>
-  `
-})
+  </div>
+  `;
+});
 
-
-
-boxCardapio.innerHTML = element.join("")
-
-
-
-
-
-
+boxCardapio.innerHTML = element.join("");
